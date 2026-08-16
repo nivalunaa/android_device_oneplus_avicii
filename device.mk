c203c4962a5946fce6d88a1cd5a905bf7f55be20
nivalunaa <197393523+nivalunaa@users.noreply.github.com>
2026-08-16 10:00:46 +0300

    avicii: Drop local audio_amplifier
    Use audio_amplifier from hardware/oplus instead.

--- device.mk
+++ device.mk
@@ -383,7 +383,8 @@ PRODUCT_SHIPPING_API_LEVEL := 29
 
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/oplus
 
 # Telephony
 PRODUCT_PACKAGES += \
